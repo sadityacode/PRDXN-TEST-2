1ed9101c791a0182ba22f58b257cb48d348dcd01
sadityaprdxn <adityas@prdxn.com>
2020-06-10 09:26:48 +0530

    web page javascript started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-/* Author: 
+'use strict';
 
-*/
+window.onload = function () {
+
+	var page_class = this.document.querySelector(".container");
+
+	if (page_class.classList.contains("loginPage")) {
+		console.log("hii");
+	}
+}
 
 
 
